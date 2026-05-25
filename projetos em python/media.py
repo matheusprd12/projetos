369510f51bfd8ae34878f04493f9c4f2d0aa642c
gustavo.prado <gustavopsz2002@gmail.com>
2026-05-25 18:55:12 -0300

    a new project, calculate your school average

---
+++ projetos em python/media.py
@@ -0,0 +1,8 @@
+# calcule sua média escolar do ano
+
+num1 = int(input("Digite a nota do primeiro bimestre: "))
+num2 = int(input("Digite a nota do segundo bimestre: "))
+num3 = int(input("Digite a nota do terceiro bimestre: "))
+num4 = int(input("Digite a nota do quarto bimestre: "))
+media = (num1 + num2 + num3 + num4) / 4
+print(f"Sua nota final é {media}")
