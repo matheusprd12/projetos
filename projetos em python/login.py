ef46cd2d914048eef930bc99460dc677b765a105
gustavo.prado <gustavopsz2002@gmail.com>
2026-05-25 19:19:13 -0300

    a new project, login and password

---
+++ projetos em python/login.py
@@ -0,0 +1,8 @@
+#login
+
+usuario = input("Digite seu usuário: ")
+senha = input("Digite sua senha: ")
+if usuario == "Matheus" and senha == "matheus123":
+    print("Acesso concedido")
+else:
+    print("Acesso negado, usuário ou senha incorreto.")
